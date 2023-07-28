d6c66fd20e6fa965fce65c97402c8ba333afd470
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-28 16:13:26 +0530

    [AkankshaAnil] creating LinkedList

--- DataStructurePP14/Program.cs
+++ DataStructurePP14/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using DataStructurePP14;
+using System.Collections.Generic;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, Let's Do The Practice Of DataStructure");
-    }
+
+        LinkedList list = new LinkedList();
+        list.Add(56);
+        list.Add(30);
+        list.Add(70);
+        list.Display();
+     }
 }
